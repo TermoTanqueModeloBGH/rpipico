db8a1e3ceb2bed6d4afbc7349d2ac1350be8699c
Julian <julianalmiron50@gmail.com>
2026-04-15 08:47:04 -0300

    implementacion de queue y suscripciones

--- main.py
+++ main.py
@@ -1,67 +1,78 @@
-# (C) Copyright Peter Hinch 2017-2019.
-# Released under the MIT licence.
-
-# This demo publishes to topic "result" and also subscribes to that topic.
-# This demonstrates bidirectional TLS communication.
-# You can also run the following on a PC to verify:
-# mosquitto_sub -h test.mosquitto.org -t result
-# To get mosquitto_sub to use a secure connection use this, offered by @gmrza:
-# mosquitto_sub -h <my local mosquitto server> -t result -u <username> -P <password> -p 8883
-
-# Public brokers https://github.com/mqtt/mqtt.github.io/wiki/public_brokers
-
-# red LED: ON == WiFi fail
-# green LED heartbeat: demonstrates scheduler is running.
-
 from mqtt_as import MQTTClient
 from mqtt_local import config
 import uasyncio as asyncio
-import dht, machine
+import dht, machine, json
+#librerias necesarias para sacar la mac
+import network
+import ubinascii
 
-d = dht.DHT11(machine.Pin(15))
+#establezco la id del dispositivo usando la mac del dispositivo
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+#obtencion y decodificacion de la direc mac
+ID_DEL_DISPOSITIVO= ubinascii.hexlify(wlan.config('mac'), ':').decode()
+print('\nMAC(colocar esto en MQTTX):')
+print(ID_DEL_DISPOSITIVO)
 
-def sub_cb(topic, msg, retained):
-    print('Topic = {} -> Valor = {}'.format(topic.decode(), msg.decode()))
+#Aca esta el sensor
+d = dht.DHT11(machine.Pin(15)) 
 
+#Para recibir los datos del MQTTX
+async def messages(client):
+    async for topic, msg, retained in client.queue:
+        print(topic.decode(), msg.decode())
+
+#este es para el funcionamiento del wifi cuando sube o baja
 async def wifi_han(state):
     print('Wifi is ', 'up' if state else 'down')
     await asyncio.sleep(1)
 
-# If you connect with clean_session True, must re-subscribe (MQTT spec 3.1.2.4)
-async def conn_han(client):
-    await client.subscribe('topico/temperatura', 1)
-    await client.subscribe('topico/humedad', 1)
+# las cosas que me interesan recibir del broker
+async def up(client):
+    while True:
+        await client.up.wait()
+        client.up.clear()
+        await client.subscribe(f'{ID_DEL_DISPOSITIVO}/setpoint',1)
+        await client.subscribe(f'{ID_DEL_DISPOSITIVO}/periodo',1)
+        await client.subscribe(f'{ID_DEL_DISPOSITIVO}/destello',1)
+        await client.subscribe(f'{ID_DEL_DISPOSITIVO}/modo',1)
+        await client.subscribe(f'{ID_DEL_DISPOSITIVO}/rele',1)
+        await client.subscribe(f'{ID_DEL_DISPOSITIVO}/temperatura',1)
+        await client.subscribe(f'{ID_DEL_DISPOSITIVO}/humedad',1)
 
 async def main(client):
     await client.connect()
-    n = 0
-    await asyncio.sleep(2)  # Give broker time
+    for coroutine in (up, messages):
+        asyncio.create_task(coroutine(client))
+    n=0
     while True:
+        await asyncio.sleep(5)
         try:
             d.measure()
-            try:
-                temperatura=d.temperature()
-                await client.publish('topico/temperatura', '{}'.format(temperatura), qos = 1)
-            except OSError as e:
-                print("sin sensor temperatura")
-            try:
-                humedad=d.humidity()
-                await client.publish('topico/humedad', '{}'.format(humedad), qos = 1)
-            except OSError as e:
-                print("sin sensor humedad")
+            temperatura=d.temperature()
+            humedad=d.humidity()
+            datos= {
+            "temperatura": temperatura,
+            "humedad": humedad,
+            "setpoint":25,
+            "modo":"auto",
+            "periodo": 5
+            }
+            conversion=json.dumps(datos)
+            await client.publish(ID_DEL_DISPOSITIVO, conversion, qos = 1)
         except OSError as e:
             print("sin sensor")
         await asyncio.sleep(20)  # Broker is slow
+        n +=1
 
 # Define configuration
-config['subs_cb'] = sub_cb
-config['connect_coro'] = conn_han
+config["queue_len"]=1 
 config['wifi_coro'] = wifi_han
-config['ssl'] = True
+config['ssl'] = True #para cifrar los datos
 
 # Set up client
 MQTTClient.DEBUG = True  # Optional
-client = MQTTClient(config)
+client = MQTTClient(config) #crea el objeto cliente
 try:
     asyncio.run(main(client))
 finally:
