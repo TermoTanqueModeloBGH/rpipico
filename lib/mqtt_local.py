a5ec2a6cac4390360ba6fce3de6a2df69c944f80
Julian <julianalmiron50@gmail.com>
2026-04-15 11:34:32 -0300

    implementacion del envio desde el MQTTX

--- lib/mqtt_local.py
+++ lib/mqtt_local.py
@@ -1,24 +1,14 @@
 # mqtt_local.py Local configuration for mqtt_as demo programs.
 from sys import platform, implementation
 from mqtt_as import config
-<<<<<<< HEAD
 from settings import SSID, password, BROKER
-=======
-from settings import SERVIDOR, SSID, PASS, USR_MQTT, PASS_MQTT
->>>>>>> 10e1769f46b0b0a580057340d2d7450f385a607b
 
 config['server'] = BROKER  # Change to suit
 #  config['server'] = 'test.mosquitto.org'
 
 # Not needed if you're only using ESP8266
 config['ssid'] = SSID
-<<<<<<< HEAD
 config['wifi_pw'] = password
-=======
-config['wifi_pw'] = PASS
-config['user'] = USR_MQTT
-config['password'] = PASS_MQTT
->>>>>>> 10e1769f46b0b0a580057340d2d7450f385a607b
 
 # For demos ensure same calling convention for LED's on all platforms.
 # ESP8266 Feather Huzzah reference board has active low LED's on pins 0 and 2.
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from machine import Pin
 from mqtt_as import MQTTClient
 from mqtt_local import config
 import uasyncio as asyncio
@@ -14,13 +15,54 @@ ID_DEL_DISPOSITIVO= ubinascii.hexlify(wlan.config('mac'), ':').decode()
 print('\nMAC(colocar esto en MQTTX):')
 print(ID_DEL_DISPOSITIVO)
 
+#Para el destello
+evento=asyncio.Event()
+
 #Aca esta el sensor
 d = dht.DHT11(machine.Pin(15)) 
+#el led de destello
+led=machine.Pin("LED", Pin.OUT)
+#pin para el rele   
+r=machine.Pin(16, Pin.OUT)
+r.value(1)
+
+#inicializacion y valores default
+estado = {
+    "setpoint": 25.0,
+    "modo": "auto", #puede ser "auto" o "manual"
+    "periodo": 25,
+    "rele_orden": False  # lo que se recibe por el topico rele
+}
 
 #Para recibir los datos del MQTTX
 async def messages(client):
     async for topic, msg, retained in client.queue:
-        print(topic.decode(), msg.decode())
+        
+        instruccion=topic.decode()
+        valor=msg.decode()
+
+        print(f"comando recibido: {instruccion}->{valor}")
+
+        if instruccion.endswith('/setpoint'):
+            estado["setpoint"]=float(valor)
+        elif instruccion.endswith('/periodo'):
+            estado["periodo"]=int(valor)
+        elif instruccion.endswith('/modo'):
+            estado["modo"]=valor.lower() # 'auto' o 'manual'
+        elif instruccion.endswith('/rele'):
+            estado["rele_orden"]=valor == "1"
+        elif instruccion.endswith('/destello'):
+            evento.set() #se activa el evento del destello
+
+async def destello():
+    while True:
+        await evento.wait()
+        evento.clear()
+        for _ in range(10):
+            led.toggle()
+            await asyncio.sleep_ms(200)
+        led.off()
+
 
 #este es para el funcionamiento del wifi cuando sube o baja
 async def wifi_han(state):
@@ -42,28 +84,37 @@ async def up(client):
 
 async def main(client):
     await client.connect()
-    for coroutine in (up, messages):
-        asyncio.create_task(coroutine(client))
-    n=0
+    for coroutine in (up, messages, destello):
+        asyncio.create_task(up(client))
+        asyncio.create_task(messages(client))
+        asyncio.create_task(destello())
     while True:
-        await asyncio.sleep(5)
         try:
             d.measure()
             temperatura=d.temperature()
             humedad=d.humidity()
+
+            if estado["modo"] =="auto":
+                if temperatura>estado["setpoint"]:
+                    r.value(0) #se enciende porque es activo en bajo
+                else:
+                    r.value(1) #apagar
+            else:
+                r.value(0 if estado["rele_orden"] else 1)
+    
             datos= {
-            "temperatura": temperatura,
-            "humedad": humedad,
-            "setpoint":25,
-            "modo":"auto",
-            "periodo": 5
+                "temperatura":temperatura,
+                "humedad":humedad,
+                "setpoint": estado["setpoint"],
+                "periodo": estado["periodo"],
+                "modo": estado["modo"]
+            
             }
             conversion=json.dumps(datos)
             await client.publish(ID_DEL_DISPOSITIVO, conversion, qos = 1)
         except OSError as e:
             print("sin sensor")
-        await asyncio.sleep(20)  # Broker is slow
-        n +=1
+        await asyncio.sleep(estado["periodo"])  # manda cada cuanto
 
 # Define configuration
 config["queue_len"]=1 
