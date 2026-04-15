84029552bf3e665a6f33df90658c584d365700fc
Julian <julianalmiron50@gmail.com>
2026-04-15 08:56:55 -0300

    queue y suscripciones

--- main.py
+++ main.py
@@ -2,13 +2,9 @@ from mqtt_as import MQTTClient
 from mqtt_local import config
 import uasyncio as asyncio
 import dht, machine, json
-<<<<<<< HEAD
 #librerias necesarias para sacar la mac
 import network
 import ubinascii
-=======
-from collections import OrderedDict
->>>>>>> 10e1769f46b0b0a580057340d2d7450f385a607b
 
 #establezco la id del dispositivo usando la mac del dispositivo
 wlan = network.WLAN(network.STA_IF)
@@ -31,7 +27,6 @@ async def wifi_han(state):
     print('Wifi is ', 'up' if state else 'down')
     await asyncio.sleep(1)
 
-<<<<<<< HEAD
 # las cosas que me interesan recibir del broker
 async def up(client):
     while True:
@@ -44,11 +39,6 @@ async def up(client):
         await client.subscribe(f'{ID_DEL_DISPOSITIVO}/rele',1)
         await client.subscribe(f'{ID_DEL_DISPOSITIVO}/temperatura',1)
         await client.subscribe(f'{ID_DEL_DISPOSITIVO}/humedad',1)
-=======
-# If you connect with clean_session True, must re-subscribe (MQTT spec 3.1.2.4)
-async def conn_han(client):
-    await client.subscribe(config['client_id'], 1)
->>>>>>> 10e1769f46b0b0a580057340d2d7450f385a607b
 
 async def main(client):
     await client.connect()
@@ -59,7 +49,6 @@ async def main(client):
         await asyncio.sleep(5)
         try:
             d.measure()
-<<<<<<< HEAD
             temperatura=d.temperature()
             humedad=d.humidity()
             datos= {
@@ -75,24 +64,6 @@ async def main(client):
             print("sin sensor")
         await asyncio.sleep(20)  # Broker is slow
         n +=1
-=======
-            try:
-                temperatura=d.temperature()
-                try:
-                    humedad=d.humidity()
-                    datos=json.dumps(OrderedDict([
-                        ('temperatura',temperatura),
-                        ('humedad',humedad)
-                    ]))
-                    await client.publish(config['client_id'], datos, qos = 1)
-                except OSError as e:
-                    print("sin sensor temperatura")
-            except OSError as e:
-                print("sin sensor humedad")
-        except OSError as e:
-            print("sin sensor")
-        await asyncio.sleep(180)  # Broker is slow
->>>>>>> 10e1769f46b0b0a580057340d2d7450f385a607b
 
 # Define configuration
 config["queue_len"]=1 
